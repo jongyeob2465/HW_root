1193c1a432dd0b4f9ed0426b898cf8c0387ce31f
jongyeob2465 <71799243+jongyeob2465@users.noreply.github.com>
2020-12-18 10:30:26 +0900

    Update p2.c

--- p2.c
+++ p2.c
@@ -9,28 +9,28 @@ void macro()
         FILE *fp ;
 
         for (i = 0 ; i<6 ; i++) { //file 작성 loop
-        sprintf(file,"ex-00%d.dat",i+1); // file 이름 작성
-        n = array1[i], p = array2[i] ; // n,p값 정의
-        fp = fopen(file,"w"); // file 작성
+                sprintf(file,"ex-00%d.dat",i+1); // file 이름 작성
+                n = array1[i], p = array2[i] ; // n,p값 정의
+                fp = fopen(file,"w"); // file 작성
 
-        for (k = 0 ; k<=n ; k++) { //k는 성공회수, n은 실행횟수이므로, k값의 범위는 n을 넘지 못한다.
-                f = (TMath::Factorial(n)/((TMath::Factorial(k))*(TMath::Factorial(n-k))))*pow(p,k)*pow(1-p,n-k); // 이항분포값을 f에 넣음
-                fprintf(fp,"%d %f \n",k,f);
+                for (k = 0 ; k<=n ; k++) { //k는 성공회수, n은 실행횟수이므로, k값의 범위는 n을 넘지 못한다.
+                        f = (TMath::Factorial(n)/((TMath::Factorial(k))*(TMath::Factorial(n-k))))*pow(p,k)*pow(1-p,n-k); // 이항분포값을 f에 넣음
+                        fprintf(fp,"%d %f \n",k,f);
+                } // f값을 파일에 넣는 루프 끝.
 
-        } // f값을 파일에 넣는 루프 끝.
+                fclose(fp) ; //파일 작성 종료
 
-        fclose(fp) ; //파일 작성 종료
+                TGraph *p = new TGraph(file); //그래프 작성
 
-        TGraph *p = new TGraph(file); //그래프 작성
-        if (i==0){
-        p->GetXaxis()->SetLimits(0,20); //그래프 x축 단위 제한
-        p->SetLineColor(array3[i]) ; //각 그래프 별로 색깔 다르게 적용
-        p->Draw();
-        }
-        else if (i != 0){
-        p->SetLineColor(array3[i]);
-        p->Draw("same");
-        }
+                if (i == 0){
+                        p->GetXaxis()->SetLimits(0,20); //그래프 x축 단위 제한
+                        p->SetLineColor(array3[i]) ; //각 그래프 별로 색깔 다르게 적용
+                        p->Draw();
+                }
+                if (i != 0){
+                        p->SetLineColor(array3[i]);
+                        p->Draw("same");
+                }
 
 
         } //파일 작성 루프 끝
